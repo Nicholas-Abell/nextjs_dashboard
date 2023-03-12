93aa9dc36b9be2703b166c59e2bc60e19317d673
Nick Abell <darkforceyoda@gmail.com>
2023-03-12 18:36:05 -0500

    Customers page Layout

---
+++ pages/orders.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FaShoppingBag } from 'react-icons/fa';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { data } from '../data/data';
+
+const Orders = () => {
+    return (
+        <div className='bg-gray-100 min-h-screen'>
+            <div className='flex justify-between px-4 pt-4'>
+                <h2 classN>Orders</h2>
+                <h2>Welcome Back, Client</h2>
+            </div>
+            <div className='p-4'>
+                <div className='w-full m-auto p-4'>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Orders
